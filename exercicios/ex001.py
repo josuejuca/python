9138e2d6de76dd9413da5162ade746fe7d1d71b2
Josué Juca <89287006+josuejuca@users.noreply.github.com>
2022-07-23 11:39:37 -0300

    Delete exercicios directory

--- exercicios/ex001.py
+++
@@ -1,5 +0,0 @@
-# Criar um programa que escreva "Olá, Mundo!" na tela.
-print('Olá, Mundo!')
-# Outra opção
-ola = 'Olá, Mundo!'
-print(ola)
--- exercicios/ex005.py
+++
@@ -1,8 +0,0 @@
-# Faça um programa que receba um número inteiro e mostre na tela seu sucessor e seu antecessor.
-n = int(input('Digite um número: ')) # Vai ler um número 
-a = n - 1 # O número que vem antes 
-s = n + 1 # O número que vem depois 
-print('Analisando o valor {}, seu antecessor é {} e seu sucessor é {}.'.format( n, a, s))
-# Outra opção
-n1 = int(input('Digite um número: ')) # Vai ler um número 
-print('Analisando o valor {}, seu antecessor é {} e seu sucessor é {}.'.format(n1, (n1-1),(n1+1)))
