f98ce9054f952d65d61b2d574bad536839a107f1
Josué Juca <89287006+josuejuca@users.noreply.github.com>
2022-07-23 14:21:59 -0300

    ✔

--- exercicios-cursoemvideo/ex002 - Respondendo ao Usuário.py
+++ exercicios-cursoemvideo/ex002 - Respondendo ao Usuário.py
@@ -2,4 +2,4 @@
 #   Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 
 nome = str(input("Informe seu nome: "))
-print("É um prazer de conhecer, {}!".format(nome))
+print("É um prazer te conhecer, {}!".format(nome))
