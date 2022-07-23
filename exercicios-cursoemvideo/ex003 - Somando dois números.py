e3effefa2fc8649aabfa84145ef5062dddb87704
Josué Juca <89287006+josuejuca@users.noreply.github.com>
2022-07-23 14:26:22 -0300

    ✔

--- exercicios-cursoemvideo/ex003 - Somando dois números.py
+++ exercicios-cursoemvideo/ex003 - Somando dois números.py
@@ -2,6 +2,6 @@
 #   Crie um programa que leia dois números e mostre a soma entre eles.
 
 n1 = int(input("Informe o primeiro número: "))
-n2 = int(input("Informe o sengundo número: "))
+n2 = int(input("Informe o segundo número: "))
 
 print("A soma entre {} e {} é igual a {}!".format(n1, n2, n1+n2))
